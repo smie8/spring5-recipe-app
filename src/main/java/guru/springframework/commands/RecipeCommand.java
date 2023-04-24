2c17112dc5538bef4707291715c1615567135db8
smie8 <sami.eriksson@yahoo.fi>
2023-04-24 10:06:16 +0300

    Add validation and show validation errors in form.

--- src/main/java/guru/springframework/commands/RecipeCommand.java
+++ src/main/java/guru/springframework/commands/RecipeCommand.java
@@ -4,7 +4,13 @@ import guru.springframework.domain.Difficulty;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import org.hibernate.validator.constraints.Range;
+import org.hibernate.validator.constraints.URL;
 
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.Size;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -16,13 +22,28 @@ import java.util.Set;
 @NoArgsConstructor
 public class RecipeCommand {
     private Long id;
+
+    @NotBlank
+    @Size(min = 3, max = 255)
     private String description;
+
+    @Min(1)
+    @Max(999)
     private Integer prepTime;
+
+    @Range(min = 1, max = 999)
     private Integer cookTime;
+
+    @Range(min = 1, max = 100)
     private Integer servings;
-    private String source;
+
+    @URL
     private String url;
+
+    @NotBlank
     private String directions;
+
+    private String source;
     private Set<IngredientCommand> ingredients = new HashSet<>();
     private Difficulty difficulty;
     private NotesCommand notes;
